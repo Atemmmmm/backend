957c24b82ec5d8e1e6fe98684ba89d89527c0365
김연진 <love_homebody@naver.com>
2023-11-24 10:05:23 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/artpro/artpro/ArtproApplication.java
@@ -0,0 +1,13 @@
+package com.artpro.artpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtproApplication.class, args);
+	}
+
+}
