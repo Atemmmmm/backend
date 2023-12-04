571d4adc4c3eb57be4cc099f48314ee6d93774d4
김연진 <love_homebody@naver.com>
2023-12-04 22:20:43 +0900

    feat: #1 로그인 필수 입력값 확인 기능 구현

--- src/main/java/com/artpro/artpro/member/dto/LoginRequest.java
+++ src/main/java/com/artpro/artpro/member/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.artpro.artpro.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,8 +10,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식만 입력할 수 있습니다.")
     private String email;
 
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication() {
