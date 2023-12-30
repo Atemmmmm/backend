f2a9705a6100a49e8594da057359946803232571
김연진 <love_homebody@naver.com>
2023-12-30 18:30:49 +0900

    fix: 인증 필터 컴포넌트 추가

--- src/main/java/com/artpro/artpro/member/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/artpro/artpro/member/jwt/JwtAuthenticationFilter.java
@@ -9,9 +9,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
