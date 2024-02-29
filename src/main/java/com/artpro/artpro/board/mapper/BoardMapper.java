75297cdc962585a9d8cda3a436424946f1b23bcd
김연진 <love_homebody@naver.com>
2024-02-29 20:38:56 +0900

    feat: #6 노래 장르 저장 기능 추가

--- src/main/java/com/artpro/artpro/board/mapper/BoardMapper.java
+++ src/main/java/com/artpro/artpro/board/mapper/BoardMapper.java
@@ -2,18 +2,19 @@ package com.artpro.artpro.board.mapper;
 
 import com.artpro.artpro.board.dto.request.CreateBoardRequest;
 import com.artpro.artpro.board.entity.Board;
-import com.artpro.artpro.global.dto.MemberDto;
+import com.artpro.artpro.member.entity.Member;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BoardMapper {
 
-    public Board mapToEntity(CreateBoardRequest dto, MemberDto memberDto, String songUrl, String imageUrl) {
+    public Board mapToEntity(CreateBoardRequest dto, Member member, String songUrl, String imageUrl) {
         return Board.builder()
                 .cover(imageUrl)
                 .song(songUrl)
                 .title(dto.title())
-                .member(memberDto.member())
+                .genre(dto.genre())
+                .member(member)
                 .build();
     }
 }
