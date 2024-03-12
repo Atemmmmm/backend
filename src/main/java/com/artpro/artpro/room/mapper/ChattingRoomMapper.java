a7c1e1d6b6b676a9c63f0dfb052eea56bd9c4d49
김연진 <love_homebody@naver.com>
2024-03-13 00:19:05 +0900

    feat: #8 채팅방 목록 반환 기능 구현

--- src/main/java/com/artpro/artpro/room/mapper/ChattingRoomMapper.java
+++ src/main/java/com/artpro/artpro/room/mapper/ChattingRoomMapper.java
@@ -2,8 +2,11 @@ package com.artpro.artpro.room.mapper;
 
 import com.artpro.artpro.board.entity.Board;
 import com.artpro.artpro.member.entity.Member;
+import com.artpro.artpro.room.dto.response.RoomResponse;
 import com.artpro.artpro.room.entity.ChattingRoom;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class ChattingRoomMapper {
@@ -13,5 +16,19 @@ public class ChattingRoomMapper {
                 .board(board)
                 .createBy(member)
                 .build();
+    }
+
+    public RoomResponse mapToRoomDto(ChattingRoom chattingRoom, Member member) {
+        // TODO: 채팅 구현 후 마지막 메시지 추가
+        return RoomResponse.builder()
+                .counterpartNickname(findCounterpart(chattingRoom, member))
+                .build();
+    }
+
+    private String findCounterpart(ChattingRoom chattingRoom, Member member) {
+        if (Objects.equals(chattingRoom.getCreateBy().getId(), member.getId())) {
+            return chattingRoom.getBoard().getMember().getNickname();
+        }
+        return chattingRoom.getCreateBy().getNickname();
     }
 }
--- src/main/java/com/artpro/artpro/room/service/ChattingRoomService.java
+++ src/main/java/com/artpro/artpro/room/service/ChattingRoomService.java
@@ -3,12 +3,15 @@ package com.artpro.artpro.room.service;
 import com.artpro.artpro.board.entity.Board;
 import com.artpro.artpro.board.exception.BoardNotFoundException;
 import com.artpro.artpro.board.repository.BoardRepository;
-import com.artpro.artpro.board.repository.ChattingRoomRepository;
+import com.artpro.artpro.room.dto.response.RoomResponse;
+import com.artpro.artpro.room.repository.ChattingRoomRepository;
 import com.artpro.artpro.member.entity.Member;
 import com.artpro.artpro.room.entity.ChattingRoom;
 import com.artpro.artpro.room.mapper.ChattingRoomMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,12 @@ public class ChattingRoomService {
                 .orElseThrow(BoardNotFoundException::new);
         ChattingRoom room = chattingRoomMapper.mapToEntity(member, board);
         chattingRoomRepository.save(room);
+    }
+
+    public List<RoomResponse> findByMemberId(Member member) {
+        List<ChattingRoom> rooms = chattingRoomRepository.findAllByCreateByOrBoard_Member(member, member);
+        return rooms.stream()
+                .map(room -> chattingRoomMapper.mapToRoomDto(room, member))
+                .toList();
     }
 }
